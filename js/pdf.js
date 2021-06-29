81b9d5dd3511d54b11ddb4970f7f04bd705e8f24
sraboncmc <dhongu2009@gmail.com>
2021-06-29 09:11:06 +0600

    Add files via upload

---
+++ js/pdf.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'myfile.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: [7, 9], orientation: 'portrait' }
+            };
+            //html2pdf().from(invoice).set(opt).save();format: [8, 8]
+			html2pdf().from(invoice).set(opt).output("bloburl").then(function (pdf) {
+				setTimeout(function () {
+					console.log(pdf);
+					//var iframe = document.getElementById('sample-pdf');
+					//iframe.src = pdf;
+					window.open(pdf, '_blank');
+				},2000);
+			});
+        })
+}
